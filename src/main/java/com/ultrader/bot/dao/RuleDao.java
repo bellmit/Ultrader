da140a6b4feaf9ed9267b1d6fef01cf9056c9895
ytx1991 <ytx1991@gmail.com>
2019-05-03 20:11:59 -0700

    Remove REST module

--- src/main/java/com/ultrader/bot/dao/RuleDao.java
+++ src/main/java/com/ultrader/bot/dao/RuleDao.java
@@ -2,7 +2,7 @@ package com.ultrader.bot.dao;
 
 import com.ultrader.bot.model.Rule;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
@@ -10,7 +10,7 @@ import javax.transaction.Transactional;
  * Rule Repository
  * @author ytx1991
  */
-@RepositoryRestResource
+@Repository
 @Transactional
 public interface RuleDao extends CrudRepository<Rule, Long> {
 }
