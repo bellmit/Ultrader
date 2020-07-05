7c78600ecd17ef99c1c638f6a3af181e1d4b74bf
ytx1991 <ytx1991@gmail.com>
2020-07-04 18:47:49 -0700

    修复dashboard交易统计

--- src/main/java/com/ultrader/bot/dao/OrderDao.java
+++ src/main/java/com/ultrader/bot/dao/OrderDao.java
@@ -21,7 +21,7 @@ public interface OrderDao extends CrudRepository<Order, String> {
     @Query(value = "SELECT * FROM TRADINGS WHERE status='filled' and close_date > ?1 and close_date < ?2 order by close_date desc",
             nativeQuery = true)
     public List<Order> findAllOrdersByDate(LocalDateTime startDate, LocalDateTime endDate);
-    @Query(value = "SELECT * FROM TRADINGS WHERE status='filled' and symbol = ?1 and close_date <= ?2 order by close_date desc limit 2",
+    @Query(value = "SELECT * FROM TRADINGS WHERE status in ('filled', 'accepted') and symbol = ?1 and close_date <= ?2 order by close_date desc limit 2",
             nativeQuery = true)
     public List<Order> findLastTradeBySymbol(String symbol, LocalDateTime sellDate);
 }
